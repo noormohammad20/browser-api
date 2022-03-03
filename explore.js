b0b3a9ec7e5d2b8087301734c052ed7848695f58
noormohammad20 <mdsaim204@gmail.com>
2022-03-03 21:48:31 +0600

    first commit

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15
+const student = {
+    name: 'jolil', friend: 'borsa', job: 'business', makeMovie: function () {
+        console.log(name)
+    }
+}
+student.makeMovie('deen rat')
+// document.getElementById('name')
+
