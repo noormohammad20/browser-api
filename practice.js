2ba46db906bfd0129559002ea6e93ecdcf21e832
noormohammad20 <mdsaim204@gmail.com>
2022-03-06 00:38:48 +0600

    some practice added

---
+++ practice.js
@@ -0,0 +1,50 @@
+/* ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। , */
+
+/* setTimeout(() => {
+    console.log('after three second i am hare!!')
+}, 3000) */
+
+/* ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও
+ */
+/* const askNumber = () => {
+    const number = prompt('please Input A Number!!!')
+     if (number) {
+        console.log(parseInt(number) + 200)
+    }
+}
+askNumber() */
+
+/* ৩. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও  */
+
+/* const askLocation = () => {
+    const href = confirm('are you sure !! you are going in our website')
+
+    if (href) {
+        console.log('https://github.com/')
+    }
+}
+askLocation() */
+
+/* ৪. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে লিখে ফেলো।  */
+
+/* cookies holo ami jei website e visit korechi sei website er modde kichu information store kore rakhe . user experiance easy korar jonno .  */
+
+/* ৫. local storage আর session storage এর মধ্যে তিনটা পার্থক্য লিখে ফেলো। ইংরেজিতে। (নিজের মতো করে লিখবে )
+ */
+
+/*
+browser user theke jei information gulo store kore rakhe. seta dui dhororner hoy . local storage and session storage . session storage e jei information gulo rakha hoy segulo oi tab jotokhon open thake totokhon i thake . website er tab close kore dile .information gulo o delete hoye jay . r local storage er vitore parmalent vabe data store kore rekhe dey.
+*/
+//[হোম ওয়ার্ক] 
+
+/* ৬. জাভাস্ক্রিপ্ট এর কোড কিভাবে ব্রাউজারের মধ্যে রান করে। দরকার হলে গুগল বা ইউটিউবে সার্চ দিয়ে একটু ভালো করে দেখে রাখো। 
+
+৭. জাভাস্ক্রিপ্ট এর কোন কোন জিনিস asynchronous সেটার একটা লিষ্ট গুগলে সার্চ দিয়ে বের করে ফেলো। এবং এই asynchronous বলতে কি বুঝায় সেটাও দেখে রাখো। 
+
+৮. event loop লুপ কি জিনিস। এই রিলেটেড পুরা জিনিসটা আজকে অনেকেই বুঝবে না। তাও আরেকবার দেখে রাখবে। 
+*/
+
+/* 
+js holo single threted language . tobe js syncronise and asyncronise duivabe kaj korte pare . js er code gulo first e call stuck e jay sekhan theke call stack asyncronise function gulo ke call back queue te pathiye dey . jkhn call stack er kaj gulo exicute kora ses hobe . tarpor event loop er maddome call back queue er modde thaka asyncronise function gulo ke ekta ekta kore call kore exicute kore. call stack er vitore kaj korar proccess ke lifo bola hoy mean last in farst out . r call back queue er vitore kaj korar proccess ke fifo bola hoy mean first in first out . 
+*/
+
